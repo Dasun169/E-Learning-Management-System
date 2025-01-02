0c6be5c14f6e8cf3ba6de4d2c1647d543462d209
Dasun169 <dasunnavindu2001@gmail.com>
2025-01-02 23:39:54 +0530

    update3

--- frontend/lms-react-app/src/components/footer/Footer.jsx
+++ frontend/lms-react-app/src/components/footer/Footer.jsx
@@ -5,23 +5,31 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
-         <div className="footer-section-logo">
-            <img src="./Images/studentHome/logo.png" alt="logo"/>
+        <div className="footer-section-logo">
+          <img src="./Images/studentHome/logo.png" alt="logo" />
         </div>
         <div className="footer-section-about">
           <h3>About Us</h3>
           <p>
-            Welcome to Lernify Education, your ultimate learning platform. We are
-            committed to delivering quality education to students worldwide.
+            Welcome to Lernify Education, your ultimate learning platform. We
+            are committed to delivering quality education to students worldwide.
           </p>
         </div>
 
         <div className="footer-section-links">
           <h3>Quick Links</h3>
-                <p><a href="/home">Home</a></p>
-                <p><a href="/course">MY Course</a></p>
-                <p><a href="/dashboard">Dash Board</a></p>
-                <p><a href="/qae">QAE</a></p>
+          <p>
+            <a href="/home">Home</a>
+          </p>
+          <p>
+            <a href="/course">MY Course</a>
+          </p>
+          <p>
+            <a href="/dashboard">Dash Board</a>
+          </p>
+          <p>
+            <a href="/qae">QAE</a>
+          </p>
         </div>
 
         <div className="footer-section-contact">
@@ -31,8 +39,8 @@ const Footer = () => {
         </div>
       </div>
       <div className="footer-bottom">
-          <p>© 2025 Group 03. All Rights Reserved.</p>
-        </div>
+        <p>© 2025 Group 03. All Rights Reserved.</p>
+      </div>
     </footer>
   );
 };
