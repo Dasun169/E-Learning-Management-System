dc2a469f4b69a9c139f0bd52250dd33792c93515
Dasun169 <dasunnavindu2001@gmail.com>
2025-01-07 23:19:52 +0530

    try to complete the details send to the email part

---
+++ lms/src/main/java/com/example/lms/Model/UserRegistrationDto.java
@@ -0,0 +1,42 @@
+package com.example.lms.Model;
+
+public class UserRegistrationDto {
+    private String email;
+    private String fullName;
+    private String userName;
+    private String contactNumber;
+
+    // Getters and setters
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+}
