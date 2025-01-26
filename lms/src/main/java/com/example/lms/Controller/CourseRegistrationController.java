54e78b798b741655aa69788c6f324d4e57bb0448
Dasun169 <dasunnavindu2001@gmail.com>
2025-01-26 20:53:31 +0530

    all done for displaying the courses according to the user

--- lms/src/main/java/com/example/lms/Controller/CourseRegistrationController.java
+++ lms/src/main/java/com/example/lms/Controller/CourseRegistrationController.java
@@ -7,15 +7,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping({"/api/courseRegistrations"})
+@CrossOrigin(origins = "http://localhost:5173")
 public class CourseRegistrationController {
 
     @Autowired
@@ -40,5 +43,10 @@ public class CourseRegistrationController {
     public ResponseEntity<List<CourseRegistration>> getRegistrationsByUserName(@PathVariable String userName) {
         List<CourseRegistration> registrations = courseRegistrationService.getAllRegistrationsByUserName(userName);
         return ResponseEntity.ok(registrations);
+    }
+
+    @GetMapping("/exists")
+    public boolean checkExistence(@RequestParam String userName, @RequestParam String courseCode) {
+        return courseRegistrationService.checkIfExists(userName, courseCode);
     }
 }
--- lms/src/main/java/com/example/lms/config/WebConfig.java
+++ lms/src/main/java/com/example/lms/config/WebConfig.java
@@ -10,10 +10,10 @@ public class WebConfig implements WebMvcConfigurer{
   @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/api/**") // Apply CORS to all endpoints
-                .allowedOrigins("http://localhost:5174") // Frontend URL (React app running on port 5174)
+                .allowedOrigins("http://localhost:5173") // Frontend URL (React app running on port 5174)
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
-                .allowedHeaders("*") // Allow all headers
-                .allowCredentials(true); // Allow sending credentials (cookies, etc.)
+                .allowedHeaders("*"); // Allow all headers
+                //.allowCredentials(true); // Allow sending credentials (cookies, etc.)
     }
   
 }
