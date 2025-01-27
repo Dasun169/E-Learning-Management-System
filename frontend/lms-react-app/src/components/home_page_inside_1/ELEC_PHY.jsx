104e4e91b9495ed717048d6000083d193db592ae
dilshanmadhuksha <madhuks-ps20020@stu.kln.ac.lk>
2025-01-27 22:12:07 +0530

    create jsx file for elec and phys

---
+++ frontend/lms-react-app/src/components/home_page_inside_1/ELEC_PHY.jsx
@@ -0,0 +1,16 @@
+
+import Header from "../header/Header.jsx";
+import Footer from "../footer/Footer.jsx";
+import ElecPhy from "./ELEC_PHY_body.jsx";
+
+const ElecPhyNew = () => {
+  return (
+    <div className="student-home-container">
+      <Header />
+      <ElecPhy />
+      <Footer />
+    </div>
+  );
+};
+
+export default ElecPhyNew;
---
+++ frontend/lms-react-app/src/components/home_page_inside_1/ELEC_PHY_body.jsx
@@ -0,0 +1,62 @@
+
+import "./css files/ELEC_PHY_body.css";
+
+const courses = [
+  {
+    code: "COSC 32133/BECS 32263",
+    name: "Full-Stack Software Development (22/23)",
+    level: "Level III",
+  },
+  {
+    code: "PMAT 32322",
+    name: "Mathematical Methods (22/23)",
+    level: "Level III",
+  },
+  {
+    code: "STAT 32672",
+    name: "Non-parametric Statistics (22/23)",
+    level: "Level III",
+  },
+  {
+    code: "STAT 32682",
+    name: "Statistical Simulations (22/23)",
+    level: "Level III",
+  },
+];
+
+const ElecPhy = () => {
+  return (
+    <div className="dashboard"> 
+      <section className="course-overview">
+        <div className="search-sort">
+          <input type="text" placeholder="Search Courses" />
+          <select>
+            <option>Level I</option>
+            <option>Level II</option>
+            <option>Level III</option>
+            <option>Level IV</option>
+          </select>
+          <select>
+            <option>ELEC</option>
+            <option>PHY</option>
+          </select>
+        </div>
+        <div className="courses">
+          {courses.map((course, index) => (
+            <div key={index} className="course-card">
+              <div className="course-thumbnail"></div>
+              <div className="course-info">
+                <h3>{course.code}</h3>
+                <p>{course.name}</p>
+                <span>{course.level}</span>
+              </div>
+              <button className="enrllment"><img src="./Images/lock.png" alt="lock"/></button>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default ElecPhy;
