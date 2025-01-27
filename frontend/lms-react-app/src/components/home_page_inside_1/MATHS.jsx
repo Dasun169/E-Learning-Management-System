50801fbbe12be777e0440d5cad618bf5165b222c
dilshanmadhuksha <madhuks-ps20020@stu.kln.ac.lk>
2025-01-27 22:09:56 +0530

    create jsx file for maths

---
+++ frontend/lms-react-app/src/components/home_page_inside_1/MATHS.jsx
@@ -0,0 +1,16 @@
+
+import Header from "../header/Header.jsx";
+import Footer from "../footer/Footer.jsx";
+import Maths from "./MATHS_body.jsx";
+
+const MathsNew = () => {
+  return (
+    <div className="student-home-container">
+      <Header />
+      <Maths />
+      <Footer />
+    </div>
+  );
+};
+
+export default MathsNew;
