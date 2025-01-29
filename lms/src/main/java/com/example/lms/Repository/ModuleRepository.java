a327b31a69761f011adf3d20660139ae4c0fff80
Dasun169 <dasunnavindu2001@gmail.com>
2025-01-29 23:39:07 +0530

    Update module post and get according to the createdTime

--- lms/src/main/java/com/example/lms/Repository/ModuleRepository.java
+++ lms/src/main/java/com/example/lms/Repository/ModuleRepository.java
@@ -1,8 +1,11 @@
 package com.example.lms.Repository;
 
 import com.example.lms.Model.Module;
+
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ModuleRepository extends MongoRepository<Module, Long> {
-
+  List<Module> findByCourseCodeOrderByCreatedDateAsc(String courseCode);
 }
