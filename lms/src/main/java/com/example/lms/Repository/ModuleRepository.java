dc984f46ad5ba1745e9f370c07c1a7261637fea2
Dasun169 <dasunnavindu2001@gmail.com>
2025-01-30 01:01:53 +0530

    update module collection

--- lms/src/main/java/com/example/lms/Repository/ModuleRepository.java
+++ lms/src/main/java/com/example/lms/Repository/ModuleRepository.java
@@ -3,10 +3,12 @@ package com.example.lms.Repository;
 import com.example.lms.Model.Module;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ModuleRepository extends MongoRepository<Module, Long> {
   List<Module> findByCourseCodeOrderByCreatedDateAsc(String courseCode);
   void deleteByHeader(String header);
+  Optional<Module> findByHeader(String header);
 }
