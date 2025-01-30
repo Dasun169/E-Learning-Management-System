ced4e6065ee3c771c4670f47c95b53fd7c9dbdbf
Dasun169 <dasunnavindu2001@gmail.com>
2025-01-30 16:14:21 +0530

    update default header

--- frontend/lms-react-app/src/components/header/Header.jsx
+++ frontend/lms-react-app/src/components/header/Header.jsx
@@ -11,11 +11,7 @@ function Header() {
           alt="logo"
         />
       </div>
-      <div className="navbar-links">
-        <button className="nav-button">Home</button>
-        <button className="nav-button">My Courses</button>
-        <button className="nav-button">Dashboard</button>
-      </div>
+
       <div className="navbar-search">
         <input type="text" placeholder="Search..." className="search-input" />
       </div>
