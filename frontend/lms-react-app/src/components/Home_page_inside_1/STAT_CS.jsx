be5c8ba2a1a76bfeb6b7abeff7d6603dc0de1420
Dasun169 <dasunnavindu2001@gmail.com>
2025-01-30 16:16:13 +0530

    update home page inside area

---
+++ frontend/lms-react-app/src/components/Home_page_inside_1/STAT_CS.jsx
@@ -0,0 +1,15 @@
+import Header from "../header/Header.jsx";
+import Footer from "../footer/Footer.jsx";
+import StatCs from "./STAT_CS_body.jsx";
+
+const StatCsNew = () => {
+  return (
+    <div className="student-home-container">
+      <Header />
+      <StatCs />
+      <Footer />
+    </div>
+  );
+};
+
+export default StatCsNew;
