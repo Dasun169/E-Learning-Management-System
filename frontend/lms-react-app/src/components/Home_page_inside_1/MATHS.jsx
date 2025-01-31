fc3d3eb558a1abf84f0a1d0a0f441ffa4215a128
kalanamissakapathum <kalanapethum563@gmail.com>
2025-01-31 11:56:39 +0530

    update home page inside part

--- frontend/lms-react-app/src/components/Home_page_inside_1/MATHS.jsx
+++ frontend/lms-react-app/src/components/Home_page_inside_1/MATHS.jsx
@@ -1,12 +1,17 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
 import Header from "../header/Header.jsx";
 import Footer from "../footer/Footer.jsx";
 import Maths from "./MATHS_body.jsx";
 
 const MathsNew = () => {
+  const location = useLocation();
+  const userName = location.state?.username || "Guest"; // Default to "Guest" if userName is not provided
+
   return (
     <div className="student-home-container">
       <Header />
-      <Maths />
+      <Maths userName={userName} />
       <Footer />
     </div>
   );
