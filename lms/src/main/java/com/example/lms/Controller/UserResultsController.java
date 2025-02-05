1f845d826755f2b2cfff4af9584e99d02418776b
Dasun169 <dasunnavindu2001@gmail.com>
2025-02-05 23:34:48 +0530

    update the userResultController part for access backend from frontend

---
+++ lms/src/main/java/com/example/lms/Controller/UserResultsController.java
@@ -0,0 +1,33 @@
+package com.example.lms.Controller;
+
+import com.example.lms.Model.UserResults;
+import com.example.lms.Service.UserResultsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/userResults")
+@CrossOrigin(origins = "http://localhost:5173")
+public class UserResultsController {
+
+    @Autowired
+    private UserResultsService userResultsService;
+
+    @PostMapping
+    public ResponseEntity<UserResults> createUserResult(@RequestBody UserResults userResult) {
+        return ResponseEntity.ok(userResultsService.createUserResult(userResult));
+    }
+
+    @GetMapping("/userName/{userName}")
+    public ResponseEntity<List<UserResults>> getUserResultsByUserName(@PathVariable String userName) {
+        return ResponseEntity.ok(userResultsService.getUserResultsByUserName(userName));
+    }
+
+    @PutMapping("/update/{userName}")
+    public ResponseEntity<UserResults> updateUserResult(@PathVariable String userName, @RequestParam String result) {
+        UserResults updatedResult = userResultsService.updateUserResult(userName, result);
+        return updatedResult != null ? ResponseEntity.ok(updatedResult) : ResponseEntity.notFound().build();
+    }
+}
