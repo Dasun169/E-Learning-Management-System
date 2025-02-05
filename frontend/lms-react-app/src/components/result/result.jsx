e1ebe5b55ccc03b05883cad4c4fd23d1c2ce00a9
Dasun169 <dasunnavindu2001@gmail.com>
2025-02-05 23:22:12 +0530

    update result.jsx file

--- frontend/lms-react-app/src/components/result/result.jsx
+++ frontend/lms-react-app/src/components/result/result.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import Header from "../header/Header.jsx";
 import Footer from "../footer/Footer.jsx";
 import ResultBody from "./result_body.jsx";
 import "./css files/result.css";
 
 function ResultPage() {
+  const location = useLocation();
+  const { username } = location.state || {};
   return (
     <div className="result-page-inside-container">
       <Header />
-      <ResultBody />
+      <ResultBody username={username} />
       <Footer />
     </div>
   );
