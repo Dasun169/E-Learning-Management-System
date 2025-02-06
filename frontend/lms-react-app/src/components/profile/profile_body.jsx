157b1371d6e4bb3f4d1d7d521bc0d5618f2a392e
dilshanmadhuksha <madhuks-ps20020@stu.kln.ac.lk>
2025-02-06 13:30:19 +0530

    right div

--- frontend/lms-react-app/src/components/profile/profile_body.jsx
+++ frontend/lms-react-app/src/components/profile/profile_body.jsx
@@ -14,6 +14,27 @@ function ProfileBody() {
                     <a href="#">Course Result</a>
                 </nav>
             </div>
+            <div className="edit-section">
+          <h3>Edit Profile</h3>
+          <form>
+            <label>User Name</label>
+            <input type="text" defaultValue="Dilshan Madhuksha" />
+
+            <label>Password</label>
+            <input type="password" defaultValue="@^#&%@&@" />
+
+            <label>Full Name</label>
+            <input type="text" defaultValue="Adhalisla.aihd" />
+
+            <label>Email</label>
+            <input type="email" defaultValue="Adhalisla.aihd@gmail.com" />
+
+            <label>Contact</label>
+            <input type="text" defaultValue="992688585" />
+
+            <button type="submit">Edit Profile</button>
+            </form>
+            </div>
 
         </div>
     </div>
