18d3a3c1b0e94ac0d173763be4a59ba79d32196c
dilshanmadhuksha <madhuks-ps20020@stu.kln.ac.lk>
2025-02-06 13:27:15 +0530

    connet profile body with profile

--- frontend/lms-react-app/src/components/profile/profile.jsx
+++ frontend/lms-react-app/src/components/profile/profile.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from "../header/Header.jsx";
 import Footer from "../footer/Footer.jsx";
+import ProfileBody from './profile_body.jsx';
 
 function Profile() {
   return (
     <div>
        <Header />
+       <ProfileBody />
        <Footer /> 
     </div>
   )
