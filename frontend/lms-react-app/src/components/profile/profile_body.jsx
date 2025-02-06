24c1b1b147dc140cddf05afe9a91aed57318b66a
dilshanmadhuksha <madhuks-ps20020@stu.kln.ac.lk>
2025-02-06 13:28:13 +0530

    add left div

--- frontend/lms-react-app/src/components/profile/profile_body.jsx
+++ frontend/lms-react-app/src/components/profile/profile_body.jsx
@@ -5,6 +5,15 @@ function ProfileBody() {
   return (
     <div className="inside">
         <div className="inside-inside">
+            <div className="profile-section">
+            <img src="./Images/ProPic.png" alt="Profile Icon" className="profile-img" />
+            <h2>Dilshan Madhuksha</h2>
+            <nav>
+                <a href="#">Your Courses</a>
+                <a href="#">Course Enrollment</a>
+                <a href="#">Course Result</a>
+            </nav>
+            </div>
 
         </div>
     </div>
