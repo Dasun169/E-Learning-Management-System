5960b42648ddcd6571cd814eaaec01af7f36be80
Dasun169 <dasunnavindu2001@gmail.com>
2025-02-10 00:45:20 +0530

    update backend classes for update history

---
+++ lms/src/main/java/com/example/lms/Service/AdminHistoryService.java
@@ -0,0 +1,5 @@
+package com.example.lms.Service;
+
+public interface AdminHistoryService {
+
+}
---
+++ lms/src/main/java/com/example/lms/Service/Impl/AdminHistoryServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.lms.Service.Impl;
+
+import com.example.lms.Service.AdminHistoryService;
+
+public class AdminHistoryServiceImpl implements AdminHistoryService {
+
+}
