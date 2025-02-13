9205e10052b9816ad08a703859eea003c0b0fcb0
kalanamissakapathum <kalanapethum563@gmail.com>
2025-02-13 13:08:08 +0530

    rechecked the code

--- lms/src/main/java/com/example/lms/config/WebConfig.java
+++ lms/src/main/java/com/example/lms/config/WebConfig.java
@@ -1,19 +1,20 @@
 package com.example.lms.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
+public class WebConfig implements WebMvcConfigurer {
 
   @Override
-    public void addCorsMappings(@NonNull CorsRegistry registry) {
-        registry.addMapping("/api/**") // Apply CORS to all endpoints
-                .allowedOrigins("http://localhost:5173") // Frontend URL (React app running on port 5174)
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
-                .allowedHeaders("*"); // Allow all headers
-                //.allowCredentials(true); // Allow sending credentials (cookies, etc.)
-    }
-  
+  public void addCorsMappings(@NonNull CorsRegistry registry) {
+    registry.addMapping("/api/**")
+        .allowedOrigins("http://localhost:5173")
+        .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowedHeaders("*");
+
+  }
+
 }
