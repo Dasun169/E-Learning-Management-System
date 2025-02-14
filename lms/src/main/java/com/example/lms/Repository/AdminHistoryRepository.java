06004f9850d29c834b8e4499119fcc8874f24757
Dasun169 <dasunnavindu2001@gmail.com>
2025-02-14 13:47:02 +0530

    update AdminHistory collection

--- lms/src/main/java/com/example/lms/Repository/AdminHistoryRepository.java
+++ lms/src/main/java/com/example/lms/Repository/AdminHistoryRepository.java
@@ -1,5 +1,12 @@
 package com.example.lms.Repository;
 
-public interface AdminHistoryRepository {
+import com.example.lms.Model.AdminHistory;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface AdminHistoryRepository extends MongoRepository<AdminHistory, String> {
+    List<AdminHistory> findAllByOrderByActionTimeAsc(); 
 }
