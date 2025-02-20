d242d52d442201bb2d8f68570b53e7f8abf05222
kalanamissakapathum <kalanapethum563@gmail.com>
2025-02-20 09:43:17 +0530

    rechecked the code

--- frontend/lms-react-app/src/components/Home_page_inside_1/ZOOL.jsx
+++ frontend/lms-react-app/src/components/Home_page_inside_1/ZOOL.jsx
@@ -6,7 +6,7 @@ import Zool from "./ZOOL_body";
 
 const ZOOL_New = () => {
   const location = useLocation();
-  const userName = location.state?.username || "Guest"; // Default to "Guest" if userName is not provided
+  const userName = location.state?.username || "Guest"; 
   const role = location.state?.role || "Student";
 
   return (
