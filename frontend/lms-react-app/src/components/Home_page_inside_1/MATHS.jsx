e916d9774cf2558ed5bfe7e24cc846a0aca01b54
kalanamissakapathum <kalanapethum563@gmail.com>
2025-02-20 09:42:23 +0530

    rechecked the code

--- frontend/lms-react-app/src/components/Home_page_inside_1/MATHS.jsx
+++ frontend/lms-react-app/src/components/Home_page_inside_1/MATHS.jsx
@@ -6,7 +6,7 @@ import Maths from "./MATHS_body";
 
 const MathsNew = () => {
   const location = useLocation();
-  const userName = location.state?.username || "Guest"; // Default to "Guest" if userName is not provided
+  const userName = location.state?.username || "Guest"; 
   const role = location.state?.role || "Student";
 
   return (
