8bb9b75f572db35c48e61d9121cb8d1e6942af72
kalanamissakapathum <kalanapethum563@gmail.com>
2025-02-20 09:42:18 +0530

    rechecked the code

--- frontend/lms-react-app/src/components/Home_page_inside_1/IM.jsx
+++ frontend/lms-react-app/src/components/Home_page_inside_1/IM.jsx
@@ -6,7 +6,7 @@ import IM from "./IM_body";
 
 const IMNew = () => {
   const location = useLocation();
-  const userName = location.state?.username || "Guest"; // Default to "Guest" if userName is not provided
+  const userName = location.state?.username || "Guest"; 
   const role = location.state?.role || "Student";
 
   return (
